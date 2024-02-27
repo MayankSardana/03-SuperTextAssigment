96272dfbe2e908dfa4da25adec78af68362aa2cc
mayank <mayanksardana4848@gmail.com>
2024-02-27 18:26:40 +0530

    ok

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
-
-
 import express from 'express';
 import cors from 'cors';
 import fetch from 'node-fetch';
 
 
 const app = express();
-const PORT = process.env.PORT || 7000
+const PORT = process.env.PORT || 9000
 const YOUR_SECRET_API_TOKEN = 'patIHWbWN4xUjyUww.4287b75d4d56976c5dcc1d52839aaf1d47d62dfd70a9d2534450b570d27bf853';
 const BASE_ID = 'appWaP6gqg97rFvY7';
 const TABLE_NAME = 'IPL';
 
 app.use(cors());
-
 
 async function getAirtableData() {
     const endpoint = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
@@ -27,7 +24,7 @@ async function getAirtableData() {
             throw new Error(`Failed to fetch data: ${response.statusText}`);
         }
         const data = await response.json();
-    return data.records.map(record => record.fields);
+    return data.records;
 
     }catch(error)
     {
@@ -38,105 +35,47 @@ async function getAirtableData() {
 
 async function getQuestions() {
     const records = await getAirtableData();
-    return records.map((rec) => rec.Question);
+     return records;
 }
-
-app.get('/qq' , async(req , res)=>{
-    const keywords = await getQuestions();
-    let {q} = req.query;
-    return res.send(keywords[q])
-})
-
-
-app.get('/all', async(req, res) => {
-    try {
-        const keywords = await getAirtableData();
-        res.send(keywords);
-    } catch (error) {
-        console.error('Error', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
 
 app.get('/question' , async(req , res)=>{
     let {q} = req.query;
-    try{
-        const records = await getAirtableData();
-        // console.log(records[q].Question)
-        let myq = records[q];
-        res.json(myq['Question']);
-    }catch(error){
-    res.send("INTERNAL SERVER ERROR");
-    }
+    const keywords = await getAirtableData();    
+    return res.send(keywords[q].fields.Question);
 })
-app.get('/custom' , async(req , res)=>{
-    let {q} = req.query;
-    try{
-        const records = await getAirtableData();
-        // console.log(records[q].Question)
-        res.send(records[q].toString());
-    }catch(error){
-    res.send("INTERNAL SERVER ERROR");
-    }
-})
+
 
 app.get('/optionA' , async(req , res)=>{
     let {q} = req.query;
-    try{
-        const records = await getAirtableData();
-        let myq = records[q];
-        res.send(myq['OptionA'].toString());
-    }catch(error){
-    res.send("INTERNAL SERVER ERROR");
-    }
+    const keywords = await getAirtableData();    
+    return res.send(keywords[q].fields.OptionA);
 })
+
 app.get('/optionB' , async(req , res)=>{
     let {q} = req.query;
-    try{
-        const records = await getAirtableData();
-        let myq = records[q];
-        res.send(myq['OptionB'].toString());
-    }catch(error){
-    res.send("INTERNAL SERVER ERROR");
-    }
+    const keywords = await getAirtableData();    
+    return res.send(keywords[q].fields.OptionB);
 })
+
 app.get('/optionC' , async(req , res)=>{
     let {q} = req.query;
-    try{
-        const records = await getAirtableData();
-        let myq = records[q];
-        res.send(myq['OptionC'].toString());
-    }catch(error){
-    res.send("INTERNAL SERVER ERROR");
-    }
+    const keywords = await getAirtableData();    
+    return res.send(keywords[q].fields.OptionC);
 })
 app.get('/optionD' , async(req , res)=>{
     let {q} = req.query;
-    try{
-        const records = await getAirtableData();
-        let myq = records[q];
-        res.send(myq['OptionD'].toString());
-    }catch(error){
-    res.send("INTERNAL SERVER ERROR");
-    }
+    const keywords = await getAirtableData();    
+    return res.send(keywords[q].fields.OptionD);
 })
-app.get('/Solution' , async(req , res)=>{
+
+app.get('/solution' , async(req , res)=>{
     let {q} = req.query;
-    try{
-        const records = await getAirtableData();
-        let myq = records[q];
-        res.send(myq['Solution'].toString());
-    }catch(error){
-    res.send("INTERNAL SERVER ERROR");
-    }
+    const keywords = await getAirtableData();    
+    return res.send(keywords[q].fields.Solution);
 })
 
 
-
-
-
-
+ 
 app.get('/', (req, res) => {
     res.send('Hello from the Airtable data server');
 });
